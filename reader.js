41e6fe8bbe0affe7873777d62124d655dfc4ceba
Kai Marshland <kaimarshland@gmail.com>
2017-09-08 23:05:17 -0700

    Made a subscription to influx

--- reader.js
+++ reader.js
@@ -1,7 +1,24 @@
+const InfluxSubscriber = require('influx-subscriber');
+const server = new InfluxSubscriber();
 
-
-function init() {
+function init(wss) {
     console.log('Initializing nasanov-reader');
+
+    let clients = [];
+
+    // listen for client connections
+    wss.on('connection', function (ws) {
+        clients.push(ws);
+    });
+
+    // listen for new data
+    server.on('point', function (point) {
+        console.log(JSON.stringify(point));
+
+        clients.map(function (client) {
+            client.send(JSON.stringify(point));
+        })
+    });
 }
 
 module.exports = {
