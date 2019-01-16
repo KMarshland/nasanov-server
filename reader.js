c2d09c63659d1ef8440ac5f5d1c7fe7d5f6b4c10
Allen Zhu <allenzhu@stanford.edu>
2019-01-15 16:56:12 -0800

    create retention policy

--- reader.js
+++ reader.js
@@ -64,7 +64,7 @@ function respondToIDsQuery(mission, response) {
 
     influxConnection.then(influxd => {
         influx = influxd;
-        influx.query(`CREATE RETENTION POLICY always ON "defaultdb" DURATION INF REPLICATION 1`).then(y => influx.query(`SHOW RETENTION POLICIES ON "defaultdb"`).then(x => console.log(x)));
+        influx.query(`CREATE RETENTION POLICY always ON "defaultdb" DURATION INF REPLICATION 1 DEFAULT`).then(y => influx.query(`SHOW RETENTION POLICIES ON "defaultdb"`).then(x => console.log(x)));
         return influx.getMeasurements();
 
     }).then((names) => {
